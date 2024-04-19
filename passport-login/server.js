e97332dabccb354619485984bb2d7b46a84c993f
K Harsh <harshnaidu2u@gmail.com>
2024-04-19 22:39:51 +0530

    passport: initialize, session, flash, _method, authneticate

--- passport-login/server.js
+++ passport-login/server.js
@@ -1,5 +1,14 @@
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').config()
+  }
+
 const express = require("express")
 const bcrypt = require("bcrypt")
+const passport = require("passport")
+const flash = require("express-flash")
+const session = require("express-session")
+const methodOverride = require("method-override")
+const initializePassport = require("./passport-config")
 
 const USERS = []
 const app = express()
@@ -7,23 +16,44 @@ const PORT = 3000
 
 app.set('view-engine', 'ejs')
 app.use(express.urlencoded({ extended: false }))
+app.use(flash())
+app.use(session({
+    secret: process.env.SESSION_SECRET_kEY,
+    resave: false,
+    saveUninitialized: false
+}))
+app.use(passport.initialize())
+app.use(passport.session())
+app.use(methodOverride('_method'))
 
-app.get('/', (req, res) => {
-    res.render('index.ejs', { name: 'Harsh' })
+initializePassport(passport, 
+    email => {
+        return USERS.find(user => user.email === email)
+    },
+    id => {
+        return USERS.find(user => user.id === id)
+    }
+)
+
+app.get('/', checkAuthenticated,(req, res) => {
+// console.log(req.user)
+res.render('index.ejs', { name: req.user.name })
 })
 
-app.get('/login', (req, res) => {
+app.get('/login', checkNotAuthenticated, (req, res) => {
     res.render('login.ejs')
 })
 
-app.post('login', (req, res) => {
+app.post('/login', checkNotAuthenticated, passport.authenticate('local', {
+    successRedirect: '/',
+    failureRedirect: '/login',
+    failureFlash: true
+}))
 
-})
-
-app.get('/signup', (req, res) => {
+app.get('/signup', checkNotAuthenticated, (req, res) => {
     res.render('signup.ejs')
 })
-app.post('/signup', async (req, res) => {
+app.post('/signup', checkNotAuthenticated, async (req, res) => {
     try {
         const hashedPassword = await bcrypt.hash(req.body.password, 10)
         USERS.push({
@@ -38,6 +68,27 @@ app.post('/signup', async (req, res) => {
         res.redirect('/signup')
     }
 })
+
+app.delete('/logout', (req, res) => {
+    req.logout()
+    res.redirect('/login')
+})
+
+function checkAuthenticated(req, res, next) {
+    console.log("auth",req.isAuthenticated())
+    if (req.isAuthenticated()) {
+      return next()
+    }
+  
+    res.redirect('/login')
+}
+  
+  function checkNotAuthenticated(req, res, next) {
+    if (req.isAuthenticated()) {
+      return res.redirect('/')
+    }
+    next()
+}
 
 app.listen(PORT, () => {
     console.log(`Server staerted at port: ${PORT}`)

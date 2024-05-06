7eba476f424a2d5fe3336b7beb9206a5ea6f5e12
K Harsh <harshnaidu2u@gmail.com>
2024-05-06 16:05:49 +0530

    authUser, authRole func

--- user-permission-auth/server.js
+++ user-permission-auth/server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
-const { users } = require('./data')
+const { users, ROLE } = require('./data')
+const { authUser, authRole } = require('./basicAuth')
 const projectRouter = require('./routes/projects')
 
 const PORT = 5000
@@ -12,11 +13,11 @@ app.get('/', (req, res) => {
   res.send('Home Page')
 })
 
-app.get('/dashboard', (req, res) => {
+app.get('/dashboard', authUser, (req, res) => {
   res.send('Dashboard Page')
 })
 
-app.get('/admin', (req, res) => {
+app.get('/admin', authUser, authRole(ROLE.ADMIN), (req, res) => {
   res.send('Admin Page')
 })
 
